841f7564b867c21d3995002effa2f2b3083cae98
Randy Pitcher <randypitcherii@gmail.com>
2019-05-31 18:26:59 -0500

    finished cloudtrail monitoring sql setup

--- snowflake-setup/cloudtrailMonitoringIngestion.sql
+++ snowflake-setup/cloudtrailMonitoringIngestion.sql
@@ -4,20 +4,18 @@
 // set role
 USE ROLE SNOWWATCH_ROLE;
 
-A.EVENT_DATA AS EVENT_DATA,
-A.EVENT_DATA:"awsRegion" :: STRING as EVENT_REGION_NAME,
-A.EVENT_DATA:"eventName" :: STRING as EVENT_NAME,
-A.EVENT_DATA:"eventSource" :: STRING as EVENT_SOURCE,
-A.EVENT_DATA:"eventTime" :: TIMESTAMP_TZ as EVENT_TIME,
-A.EVENT_DATA:"eventType" :: STRING as EVENT_TYPE,
-A.EVENT_DATA:"sourceIPAddress" :: STRING as EVENT_SOURCE_IP,
-A.EVENT_DATA:"userAgent" :: STRING as EVENT_USER_AGENT,
-A.EVENT_DATA:"userIdentity"."arn" :: STRING as EVENT_USER_ARN,
-
-CREATE OR REPLACE TABLE
+// CREATE TABLE
+CREATE TABLE IF NOT EXISTS
   SNOWWATCH.AWS.CLOUDTRAIL_MONITORING_LANDING_ZONE(
-    TIMESTAMP_UTC TIMESTAMP_TZ, 
-    EVENT_DATA VARIANT
+    EVENT_DATA VARIANT,
+    EVENT_REGION_NAME STRING,
+    EVENT_NAME STRING,
+    EVENT_SOURCE STRING,
+    EVENT_TIME TIMESTAMP_TZ,
+    EVENT_TYPE STRING,
+    EVENT_SOURCE_IP STRING,
+    EVENT_USER_AGENT STRING,
+    EVENT_USER_ARN STRING
   );
 
 // create pipe
@@ -29,14 +27,36 @@ AS
     SNOWWATCH.AWS.CLOUDTRAIL_MONITORING_LANDING_ZONE 
   FROM (
     SELECT 
-      $1:"Records"[0]:"eventTime" :: TIMESTAMP_TZ AS TIMESTAMP_UTC,
-      $1:"Records" AS EVENT_DATA 
+      $1 AS EVENT_DATA,
+      $1:"awsRegion" :: STRING as EVENT_REGION_NAME,
+      $1:"eventName" :: STRING as EVENT_NAME,
+      $1:"eventSource" :: STRING as EVENT_SOURCE,
+      $1:"eventTime" :: TIMESTAMP_TZ as EVENT_TIME,
+      $1:"eventType" :: STRING as EVENT_TYPE,
+      $1:"sourceIPAddress" :: STRING as EVENT_SOURCE_IP,
+      $1:"userAgent" :: STRING as EVENT_USER_AGENT,
+      $1:"userIdentity"."arn" :: STRING as EVENT_USER_ARN
     FROM 
-        @SNOWWATCH.AWS.CLOUDTRAIL_MONITORING_STAGE
+        @SNOWWATCH.AWS.SNOWWATCH_S3_STAGE/cloudtrail_monitoring/
   );
     
-// refresh pipe to catch any existing files in the stage
-ALTER PIPE SNOWWATCH.AWS.CLOUDTRAIL_MONITORING_PIPE REFRESH;
+// Copy any data that may already exist
+COPY INTO 
+  SNOWWATCH.AWS.CLOUDTRAIL_MONITORING_LANDING_ZONE 
+FROM (
+  SELECT 
+    $1 AS EVENT_DATA,
+    $1:"awsRegion" :: STRING as EVENT_REGION_NAME,
+    $1:"eventName" :: STRING as EVENT_NAME,
+    $1:"eventSource" :: STRING as EVENT_SOURCE,
+    $1:"eventTime" :: TIMESTAMP_TZ as EVENT_TIME,
+    $1:"eventType" :: STRING as EVENT_TYPE,
+    $1:"sourceIPAddress" :: STRING as EVENT_SOURCE_IP,
+    $1:"userAgent" :: STRING as EVENT_USER_AGENT,
+    $1:"userIdentity"."arn" :: STRING as EVENT_USER_ARN
+  FROM 
+    @SNOWWATCH.AWS.SNOWWATCH_S3_STAGE/cloudtrail_monitoring/
+);
 
 // NOTE: do not forget to add the sqs arn to your s3 bucket for auto_ingest support
 SELECT SYSTEM$PIPE_STATUS('SNOWWATCH.AWS.CLOUDTRAIL_MONITORING_PIPE');
