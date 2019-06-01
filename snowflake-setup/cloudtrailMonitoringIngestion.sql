2f1ab73a4e7b89356e509d67b31f62fd5444d622
Randy Pitcher <randypitcherii@gmail.com>
2019-05-31 19:19:50 -0500

    added elb monitoring script

--- snowflake-setup/cloudtrailMonitoringIngestion.sql
+++ snowflake-setup/cloudtrailMonitoringIngestion.sql
@@ -7,15 +7,15 @@ USE ROLE SNOWWATCH_ROLE;
 // CREATE TABLE
 CREATE TABLE IF NOT EXISTS
   SNOWWATCH.AWS.CLOUDTRAIL_MONITORING_LANDING_ZONE(
-    EVENT_DATA VARIANT,
-    EVENT_REGION_NAME STRING,
-    EVENT_NAME STRING,
-    EVENT_SOURCE STRING,
-    EVENT_TIME TIMESTAMP_TZ,
-    EVENT_TYPE STRING,
-    EVENT_SOURCE_IP STRING,
-    EVENT_USER_AGENT STRING,
-    EVENT_USER_ARN STRING
+    EVENT_DATA          VARIANT,
+    EVENT_REGION_NAME   STRING,
+    EVENT_NAME          STRING,
+    EVENT_SOURCE        STRING,
+    EVENT_TIME          TIMESTAMP_TZ,
+    EVENT_TYPE          STRING,
+    EVENT_SOURCE_IP     STRING,
+    EVENT_USER_AGENT    STRING,
+    EVENT_USER_ARN      STRING
   );
 
 // create pipe
@@ -27,17 +27,17 @@ AS
     SNOWWATCH.AWS.CLOUDTRAIL_MONITORING_LANDING_ZONE 
   FROM (
     SELECT 
-      $1 AS EVENT_DATA,
-      $1:"awsRegion" :: STRING as EVENT_REGION_NAME,
-      $1:"eventName" :: STRING as EVENT_NAME,
-      $1:"eventSource" :: STRING as EVENT_SOURCE,
-      $1:"eventTime" :: TIMESTAMP_TZ as EVENT_TIME,
-      $1:"eventType" :: STRING as EVENT_TYPE,
-      $1:"sourceIPAddress" :: STRING as EVENT_SOURCE_IP,
-      $1:"userAgent" :: STRING as EVENT_USER_AGENT,
-      $1:"userIdentity"."arn" :: STRING as EVENT_USER_ARN
+      $1                                  AS EVENT_DATA,
+      $1:"awsRegion" :: STRING            AS EVENT_REGION_NAME,
+      $1:"eventName" :: STRING            AS EVENT_NAME,
+      $1:"eventSource" :: STRING          AS EVENT_SOURCE,
+      $1:"eventTime" :: TIMESTAMP_TZ      AS EVENT_TIME,
+      $1:"eventType" :: STRING            AS EVENT_TYPE,
+      $1:"sourceIPAddress" :: STRING      AS EVENT_SOURCE_IP,
+      $1:"userAgent" :: STRING            AS EVENT_USER_AGENT,
+      $1:"userIdentity"."arn" :: STRING   AS EVENT_USER_ARN
     FROM 
-        @SNOWWATCH.AWS.SNOWWATCH_S3_STAGE/cloudtrail_monitoring/
+      @SNOWWATCH.AWS.SNOWWATCH_S3_STAGE/cloudtrail_monitoring/
   );
     
 // Copy any data that may already exist
@@ -45,15 +45,15 @@ COPY INTO
   SNOWWATCH.AWS.CLOUDTRAIL_MONITORING_LANDING_ZONE 
 FROM (
   SELECT 
-    $1 AS EVENT_DATA,
-    $1:"awsRegion" :: STRING as EVENT_REGION_NAME,
-    $1:"eventName" :: STRING as EVENT_NAME,
-    $1:"eventSource" :: STRING as EVENT_SOURCE,
-    $1:"eventTime" :: TIMESTAMP_TZ as EVENT_TIME,
-    $1:"eventType" :: STRING as EVENT_TYPE,
-    $1:"sourceIPAddress" :: STRING as EVENT_SOURCE_IP,
-    $1:"userAgent" :: STRING as EVENT_USER_AGENT,
-    $1:"userIdentity"."arn" :: STRING as EVENT_USER_ARN
+      $1                                  AS EVENT_DATA,
+      $1:"awsRegion" :: STRING            AS EVENT_REGION_NAME,
+      $1:"eventName" :: STRING            AS EVENT_NAME,
+      $1:"eventSource" :: STRING          AS EVENT_SOURCE,
+      $1:"eventTime" :: TIMESTAMP_TZ      AS EVENT_TIME,
+      $1:"eventType" :: STRING            AS EVENT_TYPE,
+      $1:"sourceIPAddress" :: STRING      AS EVENT_SOURCE_IP,
+      $1:"userAgent" :: STRING            AS EVENT_USER_AGENT,
+      $1:"userIdentity"."arn" :: STRING   AS EVENT_USER_ARN
   FROM 
     @SNOWWATCH.AWS.SNOWWATCH_S3_STAGE/cloudtrail_monitoring/
 );
